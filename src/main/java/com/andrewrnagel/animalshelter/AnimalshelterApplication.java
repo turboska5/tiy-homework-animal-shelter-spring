41e6f1226d3bd47bba8b234ca8b2c06a4d42bb6a
turboska5 <andrewrnagel@gmail.com>
2016-09-19 13:56:42 -0400

    v0.1: Baseline

---
+++ src/main/java/com/andrewrnagel/animalshelter/AnimalshelterApplication.java
@@ -0,0 +1,12 @@
+package com.andrewrnagel.animalshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalshelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalshelterApplication.class, args);
+	}
+}
